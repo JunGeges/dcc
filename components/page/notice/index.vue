fd70aff910c2f7fb5d087af7f4c2a2c1f393748f
jensen <g_aojun01@163.com>
2022-08-17 22:55:03 +0800

    1

--- components/page/notice/index.vue
+++ components/page/notice/index.vue
@@ -1,38 +1,51 @@
 <template>
-  <!-- 店铺公告 -->
-  <view class="diy-notice" :style="{ paddingTop: `${itemStyle.paddingTop}px`, paddingBottom: `${itemStyle.paddingTop}px` }"
-    @click="onLink(params.link)">
-    <u-notice-bar padding="10rpx 24rpx" :volume-icon="params.showIcon" :autoplay="params.scrollable"
-      :bg-color="itemStyle.background" :color="itemStyle.textColor" :list="[params.text]"></u-notice-bar>
-  </view>
+	<!-- 店铺公告 -->
+	<view class="diy-notice"
+		:style="{ paddingTop: `${itemStyle.paddingTop}px`, paddingBottom: `${itemStyle.paddingTop}px` }"
+		@click="onLink(link)">
+		<u-notice-bar padding="10rpx 24rpx" :volume-icon="params.showIcon" :autoplay="params.scrollable"
+			:bg-color="itemStyle.background" :color="itemStyle.textColor" :list="[params.text]"></u-notice-bar>
+	</view>
 </template>
 
 <script>
-  import mixin from '../mixin'
+	import mixin from '../mixin'
 
-  export default {
+	export default {
 
-    /**
-     * 组件的属性列表
-     * 用于组件自定义设置
-     */
-    props: {
-      itemStyle: Object,
-      params: Object
-    },
+		/**
+		 * 组件的属性列表
+		 * 用于组件自定义设置
+		 */
+		props: {
+			itemStyle: Object,
+			params: Object
+		},
 
-    mixins: [mixin],
+		mixins: [mixin],
 
-    /**
-     * 组件的方法列表
-     * 更新属性和数据的方法与更新页面数据的方法类似
-     */
-    methods: {
+		data() {
+			return {
+				link: {
+					type: 'PAGE',
+					param: {
+						path: 'lucky/lucky/prizeList',
+						query: {}
+					}
+				}
+			}
+		},
+
+		/**
+		 * 组件的方法列表
+		 * 更新属性和数据的方法与更新页面数据的方法类似
+		 */
+		methods: {
 
 
-    }
+		}
 
-  }
+	}
 </script>
 
 <style lang="scss" scoped>

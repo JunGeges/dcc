05f3e0a4853c6f9319ebfd7a309b22c3331d695f
jensen <g_aojun01@163.com>
2023-05-03 17:37:35 +0800

    1

---
+++ pages/merchants/protocol.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="protocol-container">
+		<view class="" v-html="protocol"></view>
+	</view>
+</template>
+
+<script>
+	import * as MerchantsApi from '@/api/merchants'
+	export default {
+		data() {
+			return {
+				protocol: '',
+			}
+		},
+		
+		onLoad() {
+			MerchantsApi.getShopProtocol().then(res => {
+				this.protocol = res.data.content
+			})
+		},
+		
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.protocol-container {
+	background-color: white;
+	height: 100vh;
+	padding: 30rpx;
+	font-size: 30rpx;
+}
+</style>
